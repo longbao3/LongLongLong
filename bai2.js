43b969e611d428d087349c9e510f4ea12ccd317a
longbao3 <84785932+longbao3@users.noreply.github.com>
2021-05-26 09:19:00 +0700

    Update bai2.js

--- bai2.js
+++ bai2.js
@@ -1,1 +1,11 @@
-// let inp = prompt("enter a string: ");
+let inp = prompt("enter a string: ");
+let temp = inp.split("");
+let a = '';
+a = temp[0].toUpperCase();
+for(i = 1 ; i < temp.length ; i++){
+    if(temp[i] == ' '){
+        temp[i+1] = temp[i+1].toUpperCase();
+    }
+    a  += temp[i]; 
+}
+console.log(a);
