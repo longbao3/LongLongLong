84080df9322a47bb56006bef964cc9ce6351828d
longbao3 <84785932+longbao3@users.noreply.github.com>
2021-05-26 09:19:34 +0700

    Update bai3.js

--- bai3.js
+++ bai3.js
@@ -1,3 +1,13 @@
-// let a = prompt("enter a array: ");
-// let temp = a.split(",");
-// console.log(temp);
+let a = prompt(" Nhap vao 1 mang canh nhau boi dau , ");
+let arr = a.split(",");
+for(i = 0; i < arr.length -1 ; i++){
+    let temp = a[i];
+    {
+        for(j = i+1 ; j < arr.length; j++ ){
+            if(arr[i] == arr[j]){
+                arr.splice(j-1,1);
+            }
+        }
+    }
+}
+console.log(arr);
